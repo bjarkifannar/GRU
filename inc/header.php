52247bdccf1947d56c7e6e2d51e0e51f38cb1bd1
Stimmikex <styrmir97@gmail.com>
2016-04-05 14:59:13 +0000

    changed the header
    change the ul and li setup and made it easier to work with responsive
    design

--- inc/header.php
+++ inc/header.php
@@ -2,13 +2,21 @@
 	<nav>
 		<ul class="nav">
 			<li>
-				<a href="index.php">Home</a>
+				<ul class="nav-left">
+					<li>
+						<a href="index.php">Home</a>
+					</li>
+				</ul>
 			</li>
 			<li>
-				<a href="#">Login</a>
-			</li>
-			<li>
-				<a href="#">Register</a>
+				<ul class="nav-right">
+					<li>
+						<a href="#">Login</a>
+					</li>
+					<li>
+						<a href="#">Register</a>
+					</li>
+				</ul>
 			</li>
 		</ul>
 	</nav>
