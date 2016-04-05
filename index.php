532bac7d42e26f14e3751159328c9bc98ccc77e9
bjarkifannar <bjarkifannar@gmail.com>
2016-04-05 14:21:21 +0000

    Added a forum list to the index
    Connected the index to the database and added a forum list.

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
+	require_once 'core/db_connect.php';
+	
 	$pageName = "Home";
+
+	$forumQuery = "SELECT id, forum_name FROM forums";
+	$forumRes = $db->prepare($forumQuery);
+	$forumRes->execute();
 ?>
 <!DOCTYPE html>
 <html>
@@ -8,6 +14,30 @@
 	</head>
 	<body>
 		<?php require_once 'inc/header.php'; ?>
+		<table class="forum-list-table">
+			<thead>
+				<tr>
+					<th>
+						<h2>Forums:</h2>
+					</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+					while ($row = $forumRes->fetch(PDO::FETCH_ASSOC)) {
+				?>
+				<tr>
+					<td>
+						<h3><?php echo $row['forum_name']; ?></h3>
+					</td>
+				</tr>
+				<?php
+					}
+
+					$forumRes = null;
+				?>
+			</tbody>
+		</table>
 		<?php require_once 'inc/footer.php'; ?>
 	</body>
 </html>
