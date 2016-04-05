3cb093cccf4b13fc65be7ed2c024bf542d5d16ea
Stimmikex <styrmir97@gmail.com>
2016-04-05 13:54:05 +0000

    made some INC and header
    created the header and the folder called inc for future use

---
+++ inc/header.php
@@ -0,0 +1,15 @@
+<header>
+	<nav>
+		<ul>
+			<li>
+				<a href="index.php">Hone</a>
+			</li>
+			<li>
+				<a href="#">Login</a>
+			</li>
+			<li>
+				<a href="#">Register</a>
+			</li>
+		</ul>
+	</nav>
+</header>
