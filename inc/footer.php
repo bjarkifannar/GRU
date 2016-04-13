ab2bd9fd0d5cc3653016360ce1508435e931c231
stimmikex <styrmir97@gmail.com>
2016-04-13 16:38:03 +0000

    added facebook buttons to the side

--- inc/footer.php
+++ inc/footer.php
@@ -17,5 +17,7 @@
 		<p>&copy; 2016 Lokaverkefni GRU; Nemendur: Bjarki Fannar Snorrason, Guðlaugur Kjartan Þorgeirsson og Styrmir Óli Þorsteinsson</p>
 	</div>
 </footer>
+<div class="fb-like" data-share="true" data-width="450" data-show-faces="true"></div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+<script type="text/javascript" src="js/fb_api.js"></script>
 <?php $db = null; ?>
