2a22d2dc9753ea4f70ef280f84951e60e01b36f0
stimmikex <styrmir97@gmail.com>
2016-04-15 12:04:31 +0000

    testing out how to use branches
    in this update i added some media buttons and twitter feed to the footer

--- inc/footer.php
+++ inc/footer.php
@@ -3,13 +3,14 @@
 	<div class="top">
 		<div class="top-left">
 			<h2>Content</h2>
-			<p>Wow great stuff!</p>
+			<div><?php if (stripos($_SERVER['REQUEST_URI'], 'index.php')){echo '<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/StimmiKex" data-widget-id="720911492674121728">Tweets by @StimmiKex</a>';} ?></div>
 		</div>
 		<div class="top-right">
 			<h2>More content</h2>
 			<ul>
 				<li>wow</li>
 				<li>no way</li>
+				<li><div class="fb-like" data-share="true" data-width="450" data-show-faces="true"></div></li>
 			</ul>
 		</div>
 	</div>
@@ -17,7 +18,16 @@
 		<p>&copy; 2016 Lokaverkefni GRU; Nemendur: Bjarki Fannar Snorrason, Guðlaugur Kjartan Þorgeirsson og Styrmir Óli Þorsteinsson</p>
 	</div>
 </footer>
-<div class="fb-like" data-share="true" data-width="450" data-show-faces="true"></div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
 <script type="text/javascript" src="js/fb_api.js"></script>
+<script type="text/javascript" src="js/media.js" ></script>
+
+<?php
+	if (stripos($_SERVER['REQUEST_URI'], 'profile.php')){}
+	else if (stripos($_SERVER['REQUEST_URI'], 'login.php')){}
+	else if (stripos($_SERVER['REQUEST_URI'], 'register.php')){}
+	else {require_once "media-buttons.php";}
+
+ ?>
+
 <?php $db = null; ?>
---
+++ inc/media-buttons.php
@@ -0,0 +1,8 @@
+<div class="share-buttons">
+	<div class="fb-like" data-width="90" data-layout="button_count" data-action="like" data-show-faces="true" data-share="false"></div>
+	<div class="fb-share-button" data-layout="button_count" data-mobile-iframe="true"></div>
+	<a href="https://twitter.com/share" class="twitter-share-button" data-hashtags="Gnome">Tweet</a>
+	<div class="g-plus" data-action="share" data-annotation="bubble" data-height="24"></div>
+</div>
+
+
