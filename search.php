8e2102921e6f9a6fd96d868524b64b186f66fe72
stimmikex <styrmir97@gmail.com>
2016-04-29 11:52:18 +0000

    users can now see their profile image and change it.
    also added that if you search by name you can see their profile image

--- search.php
+++ search.php
@@ -42,6 +42,7 @@
 						$q = $_GET['q'];
 						$name_link = 'user.php?uid=';
 						$thread_link = 'thread.php?tid=';
+						if ($logged == "in") {$user_id = $_SESSION['user_id'];}
 
 					if ($selected_radio == 'thread') {
 						$thread_status = 'checked';
@@ -63,16 +64,22 @@
 					}
 					else if ($selected_radio == 'name') {
 						$name_status = 'checked';
-						$search_query = "SELECT username, id FROM users WHERE username LIKE :q ORDER BY username ASC";
+						$search_query = "SELECT username, id, profile_img FROM users WHERE username LIKE :q ORDER BY username ASC";
 						$queryRes = $db->prepare($search_query);
 						$queryRes->execute(['q' => "%{$q}%"]);
+
 				?>
 				<table class="search-table">
 					<tbody>
 					<?php
 
 						while ($row = $queryRes->fetch(PDO::FETCH_ASSOC)) {
-							echo "<tr><td class='search-output'><a href='".$name_link.$row['id']."'>".$row['username']."</a></td></tr>";
+							if (is_null($row['profile_img'])) {
+								echo "<tr><td class='user-image'><img src=img/default-user-image.png width='100' height='100'/></td><td class='search-output'><a href='".$name_link.$row['id']."'>".$row['username']."</a></td></tr>";
+							}
+							else {
+								echo "<tr><td class='user-image'><img src=img/".$row['profile_img']." width='100' height='100'/></td><td class='search-output'><a href='".$name_link.$row['id']."'>".$row['username']."</a></td></tr>";
+							}
 						}
 					?>
 					</tbody>
